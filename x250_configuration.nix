8e89c718cd6ccd9f5d3e207abab69969359ecb7a
Bence Fábián <begnoc@gmail.com>
2022-03-18 10:00:44 +0100

    explicit network interface

--- x250_configuration.nix
+++ x250_configuration.nix
@@ -39,6 +39,7 @@
   networking = {
     hostName = "x250"; # Define your hostname.
     wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    wireless.interfaces = [ "wlp3s0" ];
     # Configure network proxy if necessary
     # proxy.default = "http://user:password@proxy:port/";
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
